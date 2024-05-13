920b0dfb40101e7f8272a3146015b46f03036e6d
Christine Hjelmfelt <christine.hjelmfelt@gmail.com>
2024-05-13 16:17:19 -0500

    basic camera and player movement

---
+++ Scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+@onready var pivot = $CamOrigin
+@export var sens = 0.5
+@export var speed = 5.0
+@export var jump_velocity = 4.5
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens))
+		pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-70), deg_to_rad(70))
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
